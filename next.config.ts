88567347d62ad630f1b3170271cf02c92ebde064
Carlos Mongalo <carlito@Carloss-MacBook-Pro.local>
2026-03-26 18:41:04 -0700

    Fix deployment config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
+  basePath: "/nfcap-site",
+  assetPrefix: "/nfcap-site/",
 };
 
 export default nextConfig;
